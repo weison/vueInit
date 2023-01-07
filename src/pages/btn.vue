46a58d7754f457dec4608180a505629b617c3066
1192619694@qq.com <1192619694@qq.com>
2023-01-07 18:47:32 +0800

    feature(custom): 添加husky

--- src/pages/btn.vue
+++ src/pages/btn.vue
@@ -22,7 +22,7 @@
       <el-button type="primary" round>Primary</el-button>
       <el-button type="success" round>Success</el-button>
       <el-button type="info" round>Info</el-button>
-      <el-button type="warning" round>Warning</el-button>
+      <el-button type="warning" round>Warning 12</el-button>
       <el-button type="danger" round>Danger</el-button>
     </el-row>
   
