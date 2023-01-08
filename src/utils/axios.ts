e86d7c9e0ac073e88c13d747fe956bb67773777d
1192619694@qq.com <1192619694@qq.com>
2023-01-08 21:15:18 +0800

    fix(custom): component name should always be multi-word 处理

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -4,24 +4,25 @@ const service = axios.create();
 
 // Request interceptors
 service.interceptors.request.use(
-   (config: AxiosRequestConfig) => {
-    // do something
-    return config;
-   },
-   (error: any) => {
-    Promise.reject(error);
-   }
+    (config: AxiosRequestConfig) => {
+        // do something
+        return config;
+    },
+    (error: any) => {
+        Promise.reject(error);
+    },
 );
 
 // Response interceptors
 service.interceptors.response.use(
-  async (response: AxiosResponse) => {
-    // do something
-   },
-   (error: any) => {
-    // do something
-    return Promise.reject(error);
-   }
+    async (response: AxiosResponse) => {
+        // do something
+        return response;
+    },
+    (error: any) => {
+        // do something
+        return Promise.reject(error);
+    },
 );
 
 export default service;
